7862b3e22dc5847d3d992d833fec3767bfe2e038
KunYou Wang <stam987654@gmail.com>
2017-05-03 22:06:50 +0800

    Generalizing Fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
+  renderTagsField() {
+
+  }
+
   render() {
     return(
-      <div>
-        PostsNew!
-      </div>
+      <form>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
