e404f0f38b891bc30bcd4e6696a430483f645b6c
KunYou Wang <stam987654@gmail.com>
2017-09-11 21:00:48 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Blog
+
+![alt text](https://github.com/kunyow/Blog/blob/master/screenshot.gif "Taxi Map")
+
+```
+ > git clone git@github.com:kunyow/Blog.git
+ > cd Blog
+ > npm install
+ > npm start
+```
--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const CREATE_POST = 'create_post';
 export const DELETE_POST = 'delete_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=PAPERCLIP123';
+const API_KEY = '?key=PAPERCLIP12';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
